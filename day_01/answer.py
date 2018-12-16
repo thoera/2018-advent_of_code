660da24494c86b09a0c8c698e2e8829b459fc1b5
thoera <erard.thomas@gmail.com>
2018-12-16 17:41:10 +0100

    day-01

---
+++ day_01/answer.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from itertools import cycle
+
+# --- part one ---
+
+def parse_inputs(file):
+    with open(file, 'r') as f:
+        return [int(line) for line in f]
+
+inputs = parse_inputs('input.txt')
+
+def compute_frequency(inputs):
+    return sum(inputs)
+
+print(f'The answer of part 1 is: {compute_frequency(inputs)}')
+
+# --- part two ---
+
+def compute_calibrated_frequency(inputs):
+    frequency = 0
+    previous_frequencies = {frequency}
+
+    for i in cycle(inputs):
+        frequency += i
+        if frequency in previous_frequencies:
+            return frequency
+        else:
+            previous_frequencies.add(frequency)
+
+print(f'The answer of part 2 is: {compute_calibrated_frequency(inputs)}')
